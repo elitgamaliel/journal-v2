b0f75933dd4e3b26d1bcea340615bdbe743ff686
ELI GAMALIEL TARAZONA MARRUJO <eli.tarazona.contractor@bbva.com>
2023-03-01 06:31:19 -0500

    update files search

--- src/main/java/com/bbva/journalreader/buscar/Buscar.java
+++ src/main/java/com/bbva/journalreader/buscar/Buscar.java
@@ -1,110 +1,51 @@
 package com.bbva.journalreader.buscar;
 
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bbva.journalreader.leer.LecturaDiario;
 
 public class Buscar {
 
-  public DefaultTableModel modelo = new DefaultTableModel();
   public static final String DEPOSITO = "DEPÓSITO";
   public static final String RETIRO = "RETIRO";
-  public int tipoOperacion;
-  public String atm;
-  public String ruta;
-  public JLabel tipoOper;
-  public JLabel infoAtm;
 
-  public Buscar(String ruta) {
-    this.ruta = ruta;
-  }
+  public Buscar() {}
 
-  public Buscar() {
-
-  }
-
-  public Buscar(DefaultTableModel modelo, String ruta, JLabel tipoOper, JLabel infoAtm) {
-    this.tipoOper = tipoOper;
-    this.modelo = modelo;
-    this.ruta = ruta;
-    this.infoAtm = infoAtm;
-
-  }
-
-  public String getAtm() {
+  public String getAtm(MultipartFile[] archivos) {
     String atm = null;
-    atm = LecturaDiario.NumeroCajero(LecturaDiario.leerDiarios(ruta));
+    atm = LecturaDiario.NumeroCajero(LecturaDiario.leerDiarios(archivos));
     return atm;
   }
 
-  public int getTipoOperacion() {
+  public int getTipoOperacion(MultipartFile[] archivos) {
     int t = 0;
-    t = LecturaDiario.tipoOperacion(ruta);
+    t = LecturaDiario.tipoOperacion(archivos);
     return t;
   }
 
-  public int getSizeRemesa() {
+  public int getSizeRemesa(MultipartFile[] archivos) {
     int t = 0;
-    int tipo = getTipoOperacion();
+    int tipo = getTipoOperacion(archivos);
 
     if (tipo == 1) {
-      t = LecturaDiario.idx_Abastecimientos(LecturaDiario.leerDiarios(ruta)).size();
+      t = LecturaDiario.idx_Abastecimientos(LecturaDiario.leerDiarios(archivos)).size();
     } else if (tipo == 2) {
-      t = LecturaDiario.idx_AbastecimientosDeposito(LecturaDiario.leerDiarios(ruta)).size();
+      t = LecturaDiario.idx_AbastecimientosDeposito(LecturaDiario.leerDiarios(archivos)).size();
     }
 
     return t;
   }
 
-  public String getFechaAbastecimiento(int i) {
+  public String getFechaAbastecimiento(MultipartFile[] archivos,int i) {
     String res = null;
-    int tipo = getTipoOperacion();
+    int tipo = getTipoOperacion(archivos);
 
     if (tipo == 1) {
-      res = LecturaDiario.getFecha_Abastecimientos(LecturaDiario.leerDiarios(ruta)).get(i).substring(0, 17);
+      res = LecturaDiario.getFecha_Abastecimientos(LecturaDiario.leerDiarios(archivos)).get(i).substring(0, 17);
     } else if (tipo == 2) {
-      res = LecturaDiario.getFecha_AbastecimientosDeposito(LecturaDiario.leerDiarios(ruta)).get(i).substring(0, 17);
+      res = LecturaDiario.getFecha_AbastecimientosDeposito(LecturaDiario.leerDiarios(archivos)).get(i).substring(0, 17);
     }
     return res;
   }
 
-  public void infoCajero() {
-    System.out.println("method: infoCajero");
-    int tipo = getTipoOperacion();// RETIRO:1, DEPOSITOS:2, DEFAULT: 3
-    System.out.println("tipo: " + tipo);
-    int remesa = getSizeRemesa();
-    System.out.println("remesa: " + remesa);
-    if (tipo == 1) {
-      if (remesa > 0) {
-        for (int k = 0; k < getSizeRemesa(); k++) {
-          modelo.addRow(new Object[] { getFechaAbastecimiento(k) });
-        }
-        infoAtm.setText(getAtm());
-      } else {
-        JOptionPane.showMessageDialog(null, "No existen abastecimientos, verifica diarios", "Alerta",
-            JOptionPane.WARNING_MESSAGE);
-      }
-      tipoOper.setText(RETIRO);
-    } else if (tipo == 2) {
-      if (remesa > 0) {
-        for (int k = 0; k < getSizeRemesa(); k++) {
-          // pendiente
-          // tabla4.addRow(new Object[] { getFechaAbastecimiento(k) });
-        }
-        infoAtm.setText(getAtm());
-      } else {
-        JOptionPane.showMessageDialog(null, "No existen abastecimientos, verifica diarios", "Alerta",
-            JOptionPane.WARNING_MESSAGE);
-      }
-      tipoOper.setText(DEPOSITO);
-    } else if (tipo == 3) {
-      JOptionPane.showMessageDialog(null, "No se pudo leer contenido del directorio", "Alerta",
-          JOptionPane.WARNING_MESSAGE);
-    } else {
-      JOptionPane.showMessageDialog(null, "Pf verifica el contenido de tu directorio", "Alerta",
-          JOptionPane.WARNING_MESSAGE);
-    }
-  }
 }
--- src/main/java/com/bbva/journalreader/controller/JournalController.java
+++ src/main/java/com/bbva/journalreader/controller/JournalController.java
@@ -1,9 +1,16 @@
 package com.bbva.journalreader.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bbva.journalreader.buscar.Buscar;
 
 @Controller
 public class JournalController {
@@ -15,8 +22,39 @@ public class JournalController {
 	}
 
 	@PostMapping("/buscar")
-	public String buscar(Model model) {
+	public String buscar(@RequestParam("archivos") MultipartFile[] archivos, Model model) {
 		model.addAttribute("titulo", "JournalReader");
-		return "journal";
+		Buscar buscar = new Buscar();
+		List<String> fechas = new ArrayList<>();
+		int tipo = buscar.getTipoOperacion(archivos);// RETIRO:1, DEPOSITOS:2, DEFAULT: 3
+		int remesa = buscar.getSizeRemesa(archivos);
+		if (tipo == 1) {
+			if (remesa > 0) {
+				for (int k = 0; k < buscar.getSizeRemesa(archivos); k++) {
+					fechas.add(buscar.getFechaAbastecimiento(archivos, k));
+				}
+				model.addAttribute("info", buscar.getAtm(archivos));
+			} else {
+				model.addAttribute("message", "No existen abastecimientos, verifica diarios");
+			}
+			model.addAttribute("operacion", "RETIRO");
+		} else if (tipo == 2) {
+			if (remesa > 0) {
+				for (int k = 0; k < buscar.getSizeRemesa(archivos); k++) {
+					fechas.add(buscar.getFechaAbastecimiento(archivos, k));
+				}
+				model.addAttribute("info", buscar.getAtm(archivos));
+			} else {
+				model.addAttribute("message", "No existen abastecimientos, verifica diarios");
+			}
+			model.addAttribute("operacion", "DEPOSITO");
+		} else if (tipo == 3) {
+			model.addAttribute("message", "No se leyeron los archivos");
+		} else {
+			model.addAttribute("message", "Pf verifica el contenido de tus archivos");
+		}
+		model.addAttribute("fechas", fechas);
+		return "/journal";
 	}
+
 }
